7b11b3c292c99ad73dabfd9e9761441405194329
AllanChain <txsmlf@gmail.com>
2018-05-07 08:15:10 +0800

    enhanced number guessing

--- num_guess.py
+++ num_guess.py
@@ -4,8 +4,8 @@ RANGE=list('123456789')
 CHANCE=10
 SEQ=4
 MSG='Welcome to the number guessing game.\nCurrent setting of the game is %d chances for you to guess a %d digits.'%(CHANCE,SEQ)
-i,j=5,0
-print('\033[5;3%d;4%dm%s\033[0m'%(i,j,MSG))
+MSG2='Invalid input!'
+print('\033[5;33m%s\033[0m'%MSG)
 def isvalid(num):
     for i in num:
         if not i in RANGE:
@@ -14,36 +14,32 @@ def isvalid(num):
         return False
     return True
 
-def gen_num():
-    nums=random.sample(RANGE,k=SEQ)
-    return nums
-
 def judge(c,u):
     a=b=0
     for i in range(SEQ):
         if c[i]==u[i]:
-            #print(c,u,end=' ')
             a+=1
     for i in c:
         if i in u:
             b+=1
     return a,b-a
 def main():
-    cnum=gen_num()
+    cnum=random.sample(RANGE,k=SEQ)
     for i in range(CHANCE):
         unum=list(input('num: '))
         while not isvalid(unum):
-            print('Invalid input!')#,end=' ')
+            print('\033[1;35m%s\033[0m'%MSG2)
             unum=list(input('num: '))
         a,b=judge(cnum,unum)
         print('  ',a,'A',b,'B',sep='',end='   ')
         if a==SEQ:
             print('You win!')
-            return bool(input('Again?'))
+            return
         if CHANCE-1-i==0:
             print('It is ',''.join(cnum))
-            return bool(input('Again?'))
+            return
         print(CHANCE-1-i,'chances left')
-
-while main():
-    pass
+GOING=True
+while GOING:
+    main()
+    GOING=bool(input('Again?'))
