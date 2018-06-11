b9b432733665538bca59c7302fcb55bc0c67fb8e
AllanChain <txsmlf@gmail.com>
2018-06-11 19:11:00 +0800

    add a test of f string

---
+++ Tests/f_string_test.py
@@ -0,0 +1,17 @@
+class F:
+    def __init__(self,x,y):
+        self.x,self.y=x,y
+        return
+    def __str__(self):
+        return f'x:{self.x},y:{self.y}'
+    def __format__(self,s):
+        if s=='d':
+            return f'x:{int(self.x)},y:{int(self.y)}'
+        else:
+            print(s)
+            return f'x:{self.x:{s}},y:{self.y:{s}}'
+
+f=F(1.1,5.8)
+print(f'{f}')
+print(f'{f:d}')
+print(f'{f:.2f}')
