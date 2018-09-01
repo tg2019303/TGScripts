d74917d982be663cdef2cd671d85241d344d5b49
AllanChain <txsmlf@gmail.com>
2018-09-01 20:10:27 +0800

    add windows colorama support

--- Tests/color_test.py
+++ Tests/color_test.py
@@ -1,3 +1,10 @@
+
+try:
+    import colorama
+    colorama.init()
+except ImportError:
+    pass
 for i in range(8):
     for j in range(8):
-        print("\033[5;3%d;4%dmError!\033[0m"%(i,j))  
+        print("\033[2;3%d;4%dmError!\033[5;0m"%(i,j))  
+input()
